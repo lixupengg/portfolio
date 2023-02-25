1a96b51b2e154070f1d2ecb4635e757862fd1a83
pongzers <you@example.com>
2023-02-26 03:45:31 +0800

    fix: remove unneeded imports

--- apps/dashboard/src/pages/DashboardPage/index.tsx
+++ apps/dashboard/src/pages/DashboardPage/index.tsx
@@ -1,14 +1,7 @@
 // Basic Imports
 import React, { useEffect } from 'react';
-import { DndProvider, useDrag } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import { Layout } from '@stonksfi/components';
-import { useDashboardMetaContext } from '@stonksfi/hooks';
-
 import { mainConfig } from './configs/main';
-
-/* Styles */
-import styles from './index.module.scss';
 
 const DashboardPage = () => {
 	return (
