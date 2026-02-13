ed156271a5f8e1642f1cf5b28d627782845ea66c
Li Xupeng <lixupeng@Xupengs-MacBook-Pro.local>
2026-02-13 22:02:38 +0800

    asd

--- features/constants/game.ts
+++
@@ -1,4 +0,0 @@
-export const GRID_SIZE = 50;
-export const GRID_HELPER_TYPE = 'GridHelper';
-export const AXES_HELPER_TYPE = 'AxesHelper';
-export const THREE_HELPER_TYPES = [GRID_HELPER_TYPE, AXES_HELPER_TYPE];
